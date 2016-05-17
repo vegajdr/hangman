b6f5df0b3a5e65175ce9f0a956c924f7d19c01ee
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-05-17 14:07:40 -0400

    Board output addition

--- hangman.rb
+++ hangman.rb
@@ -3,7 +3,7 @@ require "pry"
 # Split in
 
 wordsdb = []
-
+board = []
 
 
 input = nil
@@ -11,16 +11,32 @@ input = nil
 # Split input words into arrays
 until input == "quit" do
   input = gets.chomp
-  if input.downcase != "quit"
-    wordsdb.push(input.split(""))
+  unless input.empty?
+    if input.downcase != "quit"
+      wordsdb.push(input.split(""))
+    end
   end
 end
 
-
+#RANDOM Function
 word = wordsdb[rand(0...wordsdb.length)]
 
 
+word.each do |letter|
+  print letter
+end
 
+# Board output -------------------------------------
+word.each do
+  board.push "_"
+end
+
+puts
+
+board.each do |letter|
+  print letter
+end
+# ----------------------------------------------------
 
 
 
