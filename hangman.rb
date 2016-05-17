e81865c1f37dcb5a948eaaba92c52754cc3bd9ed
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-05-17 17:51:40 -0400

    Almost done, need to fix attempts bug

--- hangman.rb
+++ hangman.rb
@@ -30,7 +30,7 @@ input = nil
 # end
 
 #RANDOM Function
-word = wordsdb[rand(0...wordsdb.length)]
+
 
 #Print word
 # word.each do |letter|
@@ -38,58 +38,71 @@ word = wordsdb[rand(0...wordsdb.length)]
 # end
 
 # Board creation -------------------------------------
-word.each do
-  board.push "_"
-end
-#----------------------------
-puts
+
 
 # Actual game starts here:
+done = false
+until done do
+  win = false
+  attempts = 6
+  word = []
+  word.clear
+  board.clear
+  word = wordsdb[rand(0...wordsdb.length)]
+  #binding.pry
 
-win = false
-attempts = 6
-
-
-puts "Please type in a letter to guess the word:"
-
-until board == word || attempts == 0#attempts taken do
-  guess = gets.chomp
-
-  if guesses.include?(guess)
-    puts "You've already guessed that letter, please guess again"
+  word.each do
+    board.push "_"
   end
-
-  if guess.length > 1
-    puts "Please type just *ONE* character! Are you trying to cheat?"
-  end
-
-  #if guess
-
-  i = 0
-  word.each do |l|
-    if guess == l
-      board[i] = guess
-    end
-    i += 1
-  end
-  i = 0
-  unless guesses.include?(guess) || guess.length > 1
-    attempts -= 1
-  end
-
-  board.each do |letter|
-    print letter
-  end
+  #----------------------------
   puts
-  puts "You have #{attempts} attempts left!"
-  guesses.push(guess)
-end
 
-if board == word
-  puts "You have guessed the word! You shall be proclaimed hero among many!"
-elsif attempts == 0
-  puts "You have killed the poor innocent man, hope you can sleep at night!"
-  puts "All you had to do was guess the word: #{word.join}"
-end
+  puts "Please type in a letter to guess the word:"
 
+  until board == word || attempts == 0#attempts taken do
+    guess = gets.chomp
+
+    if guesses.include?(guess)
+      puts "You've already guessed that letter, please guess again"
+    end
+
+    if guess.length > 1
+      puts "Please type just *ONE* character! Are you trying to cheat?"
+    end
+
+    #if guess
+
+    i = 0
+    word.each do |l|
+      if guess == l
+        board[i] = guess
+      end
+      i += 1
+    end
+    i = 0
+    unless (guesses.include?(guess) && word.include?(guess)) || guess.length > 1
+      attempts -= 1
+    end
+
+    board.each do |letter|
+      print letter
+    end
+    puts
+    puts "You have #{attempts} attempts left!"
+    guesses.push(guess)
+  end
+
+  if board == word
+    puts "You have guessed the word! You shall be proclaimed hero among many!"
+  elsif attempts == 0
+    puts "You have killed the poor innocent man, hope you can sleep at night!"
+    puts "All you had to do was guess the word: #{word.join}"
+  end
+
+  puts "Would you like to play again?:y/n"
+  replay = gets.chomp
+  unless replay.downcase == "y"
+    done = true
+  end
+end
 #binding.pry
