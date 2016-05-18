fed5396cd469ca0662ca36405b0235a5089c9c8e
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-05-18 16:32:51 -0400

    Single page length

--- hangman_update.rb
+++ hangman_update.rb
@@ -1,7 +1,6 @@
 require "pry"
 print "Let's play hangman!, a man's life lays on your hands"
 guessesdb = []
-
 def database_create
   db = []
   File.open("/usr/share/dict/words") do |f|
@@ -33,66 +32,65 @@ def match board, word, attempts
   end
 end
 def check_invalid_guess guessesdb, guess
-  if guessesdb.include?(guess)
+  if guessesdb.include?(guess) && guess != ""
     puts "You've already guessed that letter, please guess again"
   elsif guess.length > 1
     puts "Please type just *ONE* character! Are you trying to cheat?"
+  elsif guess == ""
+    puts "You didn't type anything, please choose a letter"
   end
 end
-def result
+def correct_guess word, guess, board
+  i = 0
+  word.each do |l|
+    if guess == l
+      board[i] = guess
+    end
+    i += 1
+  end
 end
-
+def attempts_count guessesdb, guess, word
+  unless guessesdb.include?(guess) || word.include?(guess) || guess.length > 1 || guess == ""
+    return true
+  end
+end
+def replay confirm
+  gamedone = nil
+  unless confirm.downcase == "y"
+    gamedone = true
+    return gamedone
+  end
+  gamedone
+end
+def display board
+  board.each do |letter|
+    print letter
+  end
+end
 wordsdb = database_create
 board = []
 input = nil
-# Actual game starts here:
-gamedone = false
-until gamedone do
+done = nil# Actual game starts here:
+until done do
   attempts = 6
   word = []
   board.clear
+  guessesdb.clear
   word = wordsdb.sample
   board = board_create word
-
-  puts
-
-  puts "Please type in a letter to guess the word:"
-
+  puts "\nPlease type in a letter to guess the word:"
   until win?(board, word, attempts) #== word || attempts == 0#attempts taken do
     guess = gets.chomp
     puts word.join
-
     check_invalid_guess guessesdb, guess
-
-#------- Potential method
-    i = 0
-    word.each do |l|
-      if guess == l
-        board[i] = guess
-      end
-      i += 1
-    end
-    i = 0
-    unless guessesdb.include?(guess) || word.include?(guess) || guess.length > 1
-      attempts -= 1
-    end
-#-------------------------
-
-    board.each do |letter|
-      print letter
-    end
-    puts
-    puts "You have #{attempts} attempts left!"
+    correct_guess word, guess, board
+    attempts -= 1 if attempts_count guessesdb, guess, word
+    display board
+    puts "\nYou have #{attempts} attempts left!"
     guessesdb.push(guess)
   end
-
   match board, word, attempts
-
   puts "Would you like to play again?:y/n"
-  replay = gets.chomp
-  unless replay.downcase == "y"
-    gamedone = true
-  end
-
+  confirm = gets.chomp
+  done = replay(confirm)
 end
-#binding.pry
